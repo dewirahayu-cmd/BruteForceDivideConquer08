26e4fcf0ecdaed67af689e8f917cd7719795df0f
dewirahayu-cmd <d3wi.r4hayu@gmail.com>
2026-03-13 08:49:29 +0700

    first

---
+++ mainPangkat08.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class mainPangkat08 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen: ");
+        int elemen = sc.nextInt();
+
+       pangkat08[] png = new pangkat08[elemen];
+        for (int i=0; i<elemen ;i++){
+            System.out.print("Masukkan nilai basis elemen ke-" + ( i + 1 ) + ": ");
+            int basis = sc.nextInt();
+            System.out.print("Masukkan nilai pangkat elemen ke-" + ( i + 1 ) + ": ");
+            int pangkat = sc.nextInt();
+            png[i] = new pangkat08 (basis, pangkat);
+        }
+
+        System.out.println("HASIL PANGKAT BRUTEFORCE:");
+        for (pangkat08 p : png) {
+            System.out.println (p.nilai + "^" + p.pangkat +": "+ p.pangkatBF (p.nilai, p.pangkat));
+
+        }
+
+        System.out.println("HASIL DEVIDE CONQUER:");
+        for (pangkat08 p : png) {
+            System.out.println(p.nilai + "^" + p.pangkat + ": " + p.pangkatDC(p.nilai, p.pangkat));
+        
+        }
+
+        sc.close();
+    }
+}
