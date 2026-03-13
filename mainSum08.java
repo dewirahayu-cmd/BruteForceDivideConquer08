2d2fe959e852bef70bf581d192a56340402d190f
dewirahayu-cmd <d3wi.r4hayu@gmail.com>
2026-03-13 10:37:31 +0700

    fix

--- mainSum08.java
+++ mainSum08.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class mainSum08 {
     public static void main(String[] args) {
-        
         Scanner input = new Scanner(System.in);
         System.out.print("Masukkan jumlah elemen: ");
         int elemen = input.nextInt();
